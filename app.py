fc6bcffbf370dd6f889127228e89321e562af935
sandeep yadav <sandyyadav9026908149@gmail.com>
2026-04-17 02:41:16 +0530

    Prepare app for deployment

--- app.py
+++ app.py
@@ -1,3 +1,5 @@
+import os
+
 from flask import Flask, jsonify, render_template, request
 
 from chatbot_engine import CustomerServiceChatbot
@@ -32,4 +34,5 @@ def chat():
 
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port, debug=False)
